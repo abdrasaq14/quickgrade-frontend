9270dea5d37bafcd12a851bee7e99dffa865de05
abdrasaq14 <abdrasaq14@gmail.com>
2024-02-14 13:44:24 +0100

    changed the url of the backend in the axios instance

--- src/utils/axiosInstance.tsx
+++ src/utils/axiosInstance.tsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // backend url
 const axiosInstance = axios.create({
-  baseURL: "https://api.quickgrade.onrender.com",
+  baseURL: "https://api-quickgrade.onrender.com",
   withCredentials: true,
 });
 
